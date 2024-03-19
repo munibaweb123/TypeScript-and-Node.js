cd6dfe466c852db67406f194559cb4ae18b0d1d7
munibaweb123 <159945886+munibaweb123@users.noreply.github.com>
2024-03-19 21:47:47 +0500

    Add files via upload

--- 46_laptop_object/laptop.ts
+++ 46_laptop_object/laptop.ts
@@ -1,1 +1,14 @@
+import { describe } from "node:test";
 
+/*Question 46: Enhanced Laptop Object: Construct an object for a laptop including properties make, model, year, and a method describe() that logs a sentence about the laptop.
+
+Explain & TIP: Objects can also contain functions (methods) that can perform actions using the object's properties. This introduces method definition within objects.*/
+let laptop={
+    make:"HP",
+    model:"Probook 640 GI",
+    year:"2017",
+    describe: function(){
+        console.log(`This laptop is a ${this.year} ${this.make} ${this.model}.`);
+    }
+};
+laptop.describe();
