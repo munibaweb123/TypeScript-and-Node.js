f6b331914773e25725014100368c627078163a93
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-27 17:12:00 +0500

    76

---
+++ 76_function_parameters_and_return_values/main.js
@@ -0,0 +1,8 @@
+"use strict";
+/**Question 76: Function Parameters and Return Values: Create a function that takes two numbers as parameters, adds them together, and returns the result. Show how you can call this function and log the result.
+
+Explain & TIP: Functions can take inputs (parameters) and give back an output (return value). They're like personal assistants that do a specific task for you and report back with the results. */
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+console.log(addNumbers(5, 8));
