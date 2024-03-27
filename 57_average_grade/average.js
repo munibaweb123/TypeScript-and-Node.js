6b94176b578d311ffcb1a30fb05ae3b6a913ff97
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-27 16:27:20 +0500

    57

---
+++ 57_average_grade/average.js
@@ -0,0 +1,7 @@
+"use strict";
+/*Question 57: Find the Average Grade: Given a list of grades, calculate the average grade.
+
+Explain & TIP: We can sum up all the grades and divide by the number of grades to find the average, giving us an idea of how well someone did overall.*/
+let grades = [68, 77, 87, 85, 92, 74];
+let averageGrade = grades.reduce((total, grade) => total + grade, 0) / grades.length;
+console.log(averageGrade);
