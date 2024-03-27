e0d8a0999d96eae40499d4b3adc2c9cfa1fba804
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-27 17:01:57 +0500

    58

--- 58_average_score_calculator/averagescore.js
+++ 58_average_score_calculator/averagescore.js
@@ -2,7 +2,7 @@
 /*Question 58: Average Score Calculator: Write a simple program that can take lots of scores and find their average.
 
 Explain & TIP: This program can handle any number of scores you give it, and it tells you the average score. Handy for seeing how well you did overall!*/
-// This program calculates the average of all scores given
+//This program calculates the average of all scores given
 function averageScore(...scores) {
     // Adds all scores together and divides by the number of scores
     let total = scores.reduce((sum, score) => sum + score, 0);
@@ -11,3 +11,4 @@ function averageScore(...scores) {
 // Example: finding the average of four scores
 console.log(averageScore(80, 90, 100, 70, 80)); // Shows the average score
 // We add up all the scores, then divide by how many there are.
+console.log(averageScore(50, 70, 87, 49)); //try with different set of numbers
