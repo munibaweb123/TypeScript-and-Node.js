f4f78189a13eb8ae86ad4c802d6671aee4866b9c
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-27 16:49:59 +0500

    70

---
+++ 70_let_in_loops/loops.ts
@@ -0,0 +1,14 @@
+/**Question 70: Understanding let in Loops: Write a function that uses a loop with the let keyword to print numbers from 1 to 5. Explain how the let keyword affects the visibility of the loop variable.
+
+Explain & TIP: The let keyword provides block scope in JavaScript, which means variables declared with let inside a loop are only accessible within that loop. This helps avoid unintended interference with other parts of your code. */
+function printNumberWithforLoop(){
+    // This function prints numbers from 1 to 5 using a loop
+    for(let i=1;i<=5;i++){
+        // Uses `let` for loop variable `i`
+        console.log(i)
+        
+    }
+// `i` is not accessible here, outside the loop, because it's defined with `let`
+}
+printNumberWithforLoop();
+// This shows that `let` limits `i` to the loop, preventing errors from using `i` where it's not intended.
