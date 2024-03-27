982067dccc902dfc206fa4e09df9678a5a03e461
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-27 16:54:20 +0500

    73

---
+++ 73_assigning_and_updating_variables/variable.js
@@ -0,0 +1,14 @@
+"use strict";
+/**Question 73: Assigning and Updating Variables: Create a function where you declare a variable using let and assign an initial value. Then, update its value within the same function and log both the initial and updated values.
+
+Explain & TIP: In JavaScript, you can assign a value to a variable with = and later update it as needed. This is fundamental for keeping track of changing data within your programs. */
+// for(let i=0;i<5;i++){
+//     console.log(i);
+// }
+function updateVariable() {
+    let number = 10;
+    console.log("initial value: " + number);
+    number = 20;
+    console.log("updated value: " + number);
+}
+updateVariable();
