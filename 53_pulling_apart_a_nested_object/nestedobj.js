04e9e99b50697b4cdac73dd203e17ceb36738375
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-27 16:21:45 +0500

    53

---
+++ 53_pulling_apart_a_nested_object/nestedobj.js
@@ -0,0 +1,12 @@
+"use strict";
+/*Question 53: Pulling Apart a Nested Object: Imagine you have a list inside another list that shows what a computer programmer knows, like coding languages, tools, and software frameworks. Find a way to get three specific skills from this list and show them.
+
+Explain & TIP: Breaking down a complex list into simpler parts makes it easier to work with and understand specific pieces of information. */
+let developerSkills = {
+    languages: ["HTML", "CSS", "JavaScript", "Typescript", "Python", "Typescript", "C", "C++", "C#", "Java"],
+    framework: [".Net", "Angular", "Gradle", "React", "Vue"],
+    tools: ["Git", "webpack", "Docker"]
+};
+let { languages, framework, tools } = developerSkills;
+console.log(`The specific skill set developer need at a time for web development:\n`);
+console.log(`Language: ${languages[0]} Framework: ${framework[0]} tool: ${tools[0]}`);
