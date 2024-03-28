71e07c4005de03a23a076435887f3bb1aeff73e1
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-28 06:59:14 +0500

    78

---
+++ 78_function_expression_vs_declaration/main.ts
@@ -0,0 +1,14 @@
+/**Question 78: Function Expressions vs. Function Declarations: Compare function expressions and declarations by creating one of each that performs the same task, such as squaring a number.
+
+Explain & TIP: JavaScript allows you to create functions in two ways: declarations that name the function and can be called before they're defined, and expressions that are stored in variables and are called using the variable name. */
+
+function squareDeclaration(number:number):number{
+    return number*number;
+}
+
+const squareExpression=function (number:number):number{
+    return number*number;
+}
+
+console.log(squareDeclaration(6));//36
+console.log(squareExpression(6));//36
