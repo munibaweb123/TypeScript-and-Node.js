b0da9524e851d01a84eac50e036b21d296b9489d
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-29 06:31:49 +0500

    82

---
+++ 82_length_of_string/main.js
@@ -0,0 +1,8 @@
+"use strict";
+/**Question 82: Find the Length of a String: Write a function that takes a string as an argument and returns the number of characters in the string.
+
+Explain & TIP: The .length property of a string gives you the total number of characters it contains, helping you understand more about the data you're working with. */
+function stringLength(str) {
+    return str.length;
+}
+console.log(stringLength("100 days of coding challenge!"));
---
+++ 82_length_of_string/main.ts
@@ -0,0 +1,10 @@
+/**Question 82: Find the Length of a String: Write a function that takes a string as an argument and returns the number of characters in the string.
+
+Explain & TIP: The .length property of a string gives you the total number of characters it contains, helping you understand more about the data you're working with. */
+
+// This function counts how many characters are in a string
+function stringLength(str:string){
+    return str.length;// Returns the number of characters in the string
+}
+
+console.log(stringLength("100 days of coding challenge!"));//count characters in this phraseS
