b615ab4563af8a12c5d500a87f0466780498fa6e
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-29 06:43:27 +0500

    83

---
+++ 83_converting_to_lower_and_upper_case/main.js
@@ -0,0 +1,11 @@
+"use strict";
+/**Question 83: Converting to Upper and Lower Case: Create a function that takes a string, converts it to uppercase, then to lowercase, and logs both.
+
+Explain & TIP: The .toUpperCase() and .toLowerCase() methods allow you to change the case of all letters in a string, useful for formatting or comparing strings. */
+// This function changes a string to uppercase and then to lowercase
+function convertCase(str) {
+    let upperStr = str.toLocaleUpperCase(); // Converts to uppercase
+    let lowerStr = str.toLowerCase(); // Converts to lowercase
+    console.log(`In upper case: ${upperStr},\n In lower case: ${lowerStr}`);
+}
+convertCase("The Governor House Initiative for generative Artifitial Intelligence and metaverse");
