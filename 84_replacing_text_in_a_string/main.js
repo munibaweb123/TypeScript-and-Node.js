d9bee6fb1f312dcc2e7f4a23016eb9c79f68dfd6
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-29 06:52:29 +0500

    84

---
+++ 84_replacing_text_in_a_string/main.js
@@ -0,0 +1,8 @@
+"use strict";
+/**Question 84: Replacing Text in a String: Write a function that takes a sentence and replaces all instances of the word "JavaScript" with "TypeScript".
+
+Explain & TIP: The .replace() method can search for a specific text pattern in a string and replace it with something else. By default, it only replaces the first occurrence, but you can use a global regex for multiple replacements. */
+function replacingString(str) {
+    return str.replace(/JavaScript/g, "TypeScript");
+}
+console.log(replacingString(`I learn JavaScript, and JavaScript is awesome!`));
