f793bd77d552ff3765eb90b89fd91593f23b8558
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-31 16:52:08 +0500

    88

---
+++ 88_Rounding_to_the_nearest_integer/main.ts
@@ -0,0 +1,12 @@
+/**Question 88: Rounding to the Nearest Integer: Write a function that takes a decimal number as input and returns the number rounded to the nearest integer.
+
+Explain & TIP: The Math.round() function is perfect for rounding decimals to the closest whole number. It looks at the decimal part and decides whether to round up or down. */
+
+// This function rounds a decimal number to the nearest whole number
+function roundingOff(num:number):number{
+    return Math.round(num);// Rounds the number
+}
+console.log(roundingOff(2.78));// near integer is 3
+console.log(roundingOff(5.234));// near integer is 5
+
+// This shows how the function rounds numbers either up or down.
