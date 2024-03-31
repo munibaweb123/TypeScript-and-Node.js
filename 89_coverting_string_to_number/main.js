1f2c4331057acf803e769fa8daca4cd0d0d71170
munibaweb123 <munibavirtualuni@gmail.com>
2024-03-31 17:10:27 +0500

    89

---
+++ 89_coverting_string_to_number/main.js
@@ -0,0 +1,11 @@
+"use strict";
+/**Question 89: Converting Strings to Numbers: Create a function that takes a string representing a number (like "123") and converts it into an actual number type.
+
+Explain & TIP: parseInt() for whole numbers or parseFloat() for decimals are handy for turning number-like strings into real numbers, allowing you to perform mathematical operations on them. */
+function stringToNumber(str) {
+    return parseFloat(str);
+}
+//function call
+console.log(stringToNumber("5")); // giving 5 in a string it will become number 5
+console.log(stringToNumber("6") + 5);
+console.log(stringToNumber("7.78")); // string to decimal number
