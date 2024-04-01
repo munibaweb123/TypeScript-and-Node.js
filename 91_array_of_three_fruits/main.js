b84329a15dfa9edb9aa6b1b1d446358bc7c38627
munibaweb123 <munibavirtualuni@gmail.com>
2024-04-01 23:46:26 +0500

    91

---
+++ 91_array_of_three_fruits/main.js
@@ -0,0 +1,7 @@
+"use strict";
+/**Question 91: Create an array of your three favorite fruits and add a new fruit to the end of the array.
+
+Explain & TIP: Adding elements to an array can be easily done using the .push() method. It allows you to add new items to the end of an array. */
+let arrOfThree = ["banana", "apple", "guava"];
+arrOfThree.push("grapes");
+console.log(arrOfThree);
---
+++ 91_array_of_three_fruits/main.ts
@@ -0,0 +1,10 @@
+/**Question 91: Create an array of your three favorite fruits and add a new fruit to the end of the array.
+
+Explain & TIP: Adding elements to an array can be easily done using the .push() method. It allows you to add new items to the end of an array. */
+
+// Defines an array with three favorite fruits
+let arrOfThree=["banana","apple","guava"];
+arrOfThree.push("grapes"); // Adds "grapes" to the end of the array
+
+console.log(arrOfThree); // output: [ 'banana', 'apple', 'guava', 'grapes' ]
+// This line adds a new fruit to our list of favorites.
