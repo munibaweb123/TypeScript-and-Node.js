1a05c9e6a0bd2dca228be3dc8861e0066ec308aa
munibaweb123 <munibavirtualuni@gmail.com>
2024-04-04 06:16:34 +0500

    100

---
+++ 100_square_root/main.js
@@ -0,0 +1,11 @@
+"use strict";
+/**Question 100: Use the JavaScript Math object to find the square root of 144.
+
+Explain & TIP: The Math.sqrt() function is a straightforward way to calculate the square root of any positive number, directly providing the result you need. */
+// Finds the square root of 144 using Math.sqrt()
+let sqrt = Math.sqrt(144);
+console.log(sqrt); //12
+// This line effectively calculates and shows the square root of 144.
+sqrt = Math.sqrt(64);
+console.log(sqrt); //8
+// This line effectively calculates and shows the square root of 64.
