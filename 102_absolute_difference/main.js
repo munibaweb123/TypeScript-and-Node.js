b3981a5c551f618e4ebc3c39f5b628b03a187cde
munibaweb123 <munibavirtualuni@gmail.com>
2024-04-04 07:07:37 +0500

    102

---
+++ 102_absolute_difference/main.js
@@ -0,0 +1,6 @@
+"use strict";
+/**Question 102: Calculate and log the absolute difference between the number -5 and 5.
+
+Explain & TIP: The Math.abs() function returns the absolute value of a number, which is useful for finding the magnitude of a difference without regard to direction. */
+const difference = Math.abs(-5 - 5);
+console.log(difference);
