0f8d1862929f491e13edb7f0b34f5afc1f944a60
munibaweb123 <munibavirtualuni@gmail.com>
2024-04-05 06:24:30 +0500

    103

---
+++ 103_random_boolean_value/main.js
@@ -0,0 +1,8 @@
+"use strict";
+/**Question 103: Write a function that returns a random boolean value, true or false.
+
+Explain & TIP: Generating a random boolean is a simple but useful trick. You can use Math.random() and check if it's above or below 0.5 to decide between true or false. */
+function getRandomBoolean() {
+    return Math.random() > 0.5;
+}
+console.log(getRandomBoolean());
