62b9303d5bfadf1715c31b29cf7b13abfe2400a7
munibaweb123 <munibavirtualuni@gmail.com>
2024-04-06 06:48:03 +0500

    108

---
+++ 108_compare_two_strings/main.ts
@@ -0,0 +1,11 @@
+/**Question 108: Compare two strings to check if they are identical, ignoring case sensitivity.
+
+Explain & TIP: To compare strings without considering their case (uppercase/lowercase), you can convert both strings to the same case before comparing. */
+
+function compareStrings(str1:string,str2:string):boolean{
+
+    return (str1.toUpperCase() === str2.toUpperCase())
+}
+
+console.log(compareStrings("course","Course"));
+console.log(compareStrings("EFFECT","AFFECT"));
