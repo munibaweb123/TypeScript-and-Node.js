d3b4e5d27d37550f667289905fa345ab2249545c
munibaweb123 <munibavirtualuni@gmail.com>
2024-04-09 06:28:56 +0500

    113

--- 113_check_if_map/main.js
+++ 113_check_if_map/main.js
@@ -2,7 +2,6 @@
 /**Question 113: Write a function that checks if a Map contains a key for "Canada" and logs the capital if it exists.
 
 Explain & TIP: You can check for the presence of a key in a Map with the .has() method and retrieve its value with the .get() method. */
-// Answer of Q12:
 // Creates a new Map to store countries and their capitals
 const countries = new Map();
 countries.set("USA", "Washington, D.C."); // Adds USA to the Map
@@ -10,7 +9,6 @@ countries.set("France", "Paris"); // Adds France to the Map
 countries.set("Japan", "Tokyo"); // Adds Japan to the Map
 console.log(countries);
 // Logs the Map with the countries and their capitals.
-// Answer of Q13:
 // This function checks for "Canada" in the Map and logs its capital
 function logCapitalOfCanada(countries) {
     if (countries.has("Canada")) {
--- 113_check_if_map/main.ts
+++ 113_check_if_map/main.ts
@@ -2,16 +2,14 @@
 
 Explain & TIP: You can check for the presence of a key in a Map with the .has() method and retrieve its value with the .get() method. */
 
-// Answer of Q12:
 // Creates a new Map to store countries and their capitals
 const countries = new Map<string, string>();
 countries.set("USA", "Washington, D.C."); // Adds USA to the Map
 countries.set("France", "Paris"); // Adds France to the Map
 countries.set("Japan", "Tokyo"); // Adds Japan to the Map
-
 console.log(countries);
 // Logs the Map with the countries and their capitals.
-// Answer of Q13:
+
 // This function checks for "Canada" in the Map and logs its capital
 function logCapitalOfCanada(countries: Map<string, string>): void {
     if (countries.has("Canada")) {
