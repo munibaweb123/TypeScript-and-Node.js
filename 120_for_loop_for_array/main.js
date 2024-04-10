8a446e0018867b8853cf5813f2e4afab1c9a5f35
munibaweb123 <munibavirtualuni@gmail.com>
2024-04-10 14:47:47 +0500

    120

---
+++ 120_for_loop_for_array/main.js
@@ -0,0 +1,9 @@
+"use strict";
+/**Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+Explain & TIP: The for...of loop is a modern loop introduced in ES6, designed to iterate over iterable objects like arrays, strings, Maps, NodeLists, and more, making it very readable and easy to use.
+ */
+let movieArray = ["Harry Potter", "Tom and jerry", "Barbie"];
+for (let movie of movieArray) {
+    console.log(movie);
+}
