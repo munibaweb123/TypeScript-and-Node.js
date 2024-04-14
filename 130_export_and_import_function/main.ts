332a5e514bc92aa3ecf615c871f6c6e385ff645d
munibaweb123 <munibavirtualuni@gmail.com>
2024-04-14 13:25:50 +0500

    130

---
+++ 130_export_and_import_function/main.ts
@@ -0,0 +1,10 @@
+/**Question 130: Explain how to export a function from one JavaScript file and import it into another file.
+
+Explain & TIP: In modern JavaScript development, modules 
+allow you to divide your code into separate files. This makes your code more organized and maintainable. You can export functions, objects, or primitives from one file and import them into another. */
+
+// In another file where you want to use the add function:
+import {add} from "./mathFunction"; // Outputs: 5
+
+console.log(add(2,3));
+// Demonstrates importing the add function from mathFunctions.ts and using it.
