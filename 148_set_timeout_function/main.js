ab417956b2a5727b2d080ad3ced4b0cb3fdf2866
munibaweb123 <munibavirtualuni@gmail.com>
2024-04-20 22:01:28 +0500

    148

---
+++ 148_set_timeout_function/main.js
@@ -0,0 +1,7 @@
+"use strict";
+/**Question 148: Demonstrate the use of the setTimeout() function to execute code after a delay.
+
+Explain & TIP: setTimeout() is a built-in JavaScript function that allows you to execute a piece of code after a specified delay. It's especially useful for scheduling tasks, animations, or delaying execution without blocking the main thread. */
+setTimeout(() => {
+    console.log(`This message will be shown after 2.5 seconds of delay!`);
+}, 2500);
---
+++ 148_set_timeout_function/main.ts
@@ -0,0 +1,10 @@
+/**Question 148: Demonstrate the use of the setTimeout() function to execute code after a delay.
+
+Explain & TIP: setTimeout() is a built-in JavaScript function that allows you to execute a piece of code after a specified delay. It's especially useful for scheduling tasks, animations, or delaying execution without blocking the main thread. */
+
+// Uses setTimeout to log a message after a 2.5-second delay
+setTimeout(()=>{
+    console.log(`This message will be shown after 2.5 seconds of delay!`);
+
+},2500)
+// This showcases how to use setTimeout to delay actions in your code.
