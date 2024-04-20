a4569e256a45672a1ce24837f8768feb93bcc3f1
munibaweb123 <munibavirtualuni@gmail.com>
2024-04-20 22:05:05 +0500

    147

--- 147_handle_error_in_callback/main.js
+++ 147_handle_error_in_callback/main.js
@@ -2,21 +2,25 @@
 /**Question 147: Explain how to handle errors in a callback pattern.
 
 Explain & TIP: Handling errors in a callback pattern often involves passing an error as the first argument to the callback. This convention allows the callback function to check for errors before proceeding with its execution. */
+// Function that uses a callback pattern, including error handling
 function fetchData(callback) {
+    // Simulating an error condition
     const error = new Error("failed to fetch data");
     const data = "Some data";
+    // Simulate fetching data with a chance of error
     if (Math.random() > 0.5) {
-        callback(null, data);
+        callback(null, data); // No error, data is fetched successfully
     }
     else {
-        callback(error);
+        callback(error); // Passes error to the callback
     }
 }
 fetchData((error, data) => {
     if (error) {
-        console.log(error.message);
+        console.log(error.message); // Handles the error 
     }
     else {
-        console.log(data);
+        console.log(data); // process the data
     }
 });
+// Demonstrates a callback pattern where errors are handled gracefully.
