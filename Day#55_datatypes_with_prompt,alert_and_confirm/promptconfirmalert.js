ab0f40644640fdf053fe75ac1a1807d676902d68
munibaweb123 <munibavirtualuni@gmail.com>
2024-04-27 09:09:03 +0500

    55

--- Day#55_datatypes_with_prompt,alert_and_confirm/promptconfirmalert.js
+++ Day#55_datatypes_with_prompt,alert_and_confirm/promptconfirmalert.js
@@ -4,3 +4,5 @@ let result=confirm("any question?");
 alert(result);// shows true or false
 let isBoss=confirm("Are you the Boss?");
 alert(isBoss);// shows true or false
+let name = prompt("What is your name?", "");
+alert(name);
