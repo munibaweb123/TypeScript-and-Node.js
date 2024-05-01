aef86b9295b02563db9eb1d06433d97d7dfe5919
munibaweb123 <munibavirtualuni@gmail.com>
2024-05-02 00:06:29 +0500

    day#60

--- Day#60_functions_in_JS/functionExpression.js
+++ Day#60_functions_in_JS/functionExpression.js
@@ -7,3 +7,19 @@ let sayHi = function() {
 
 func(); // Welcome to function expression    // (3) run the copy (it works)!
 sayHi(); // Welcome to function expression    //     this still works too (why wouldn't it)
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  function showOk() {
+    alert( "You agreed." );
+  }
+  
+  function showCancel() {
+    alert( "You canceled the execution." );
+  }
+  
+  // usage: functions showOk, showCancel are passed as arguments to ask
+  ask("Do you agree?", showOk, showCancel);
