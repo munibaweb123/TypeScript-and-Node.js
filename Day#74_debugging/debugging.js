6510fd905bd777590a503605d1f473e8f4e32f3a
munibaweb123 <munibavirtualuni@gmail.com>
2024-05-15 22:24:42 +0500

    day#74

---
+++ Day#74_debugging/debugging.js
@@ -0,0 +1,16 @@
+"use strict";
+/*************
+ * DEBUGGING *
+ *************/
+let age = 22;
+if (age > 20) {
+    console.log("pass");
+}
+else if (age < 20) {
+    console.log("return");
+}
+else {
+    console.log("reboot");
+}
+console.log("Muniba");
+//# sourceMappingURL=debugging.js.map
